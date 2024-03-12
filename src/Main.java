fcbcc5caf92885ff4a0c91f692bc6aa8d1211369
KranBurnies <kieran2007burns@gmail.com>
2024-03-12 14:10:49 +0000

    g

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Database database = new Database();
+        database.writeToDatabase("Algebra Practice", 30, "MultipleChoice", "Maths");
+    }
+}
